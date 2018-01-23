858f39eb1c8e83fd0a5c24e8b8bb4945ba9f2c09
Brian L. Gorman <brian@farreachinc.com>
2018-01-22 23:46:48 -0600

    New Autolot source imported with history

---
+++ algorithmcodesrc/Truck.java
@@ -0,0 +1,12 @@
+
+public class Truck extends Vehicle
+{
+	public Truck() {
+		
+	}
+	
+	public Truck(String startVin, String startMake, String startModel, int startYear, String startColor, double startMileage)
+	{
+		super(startVin, startMake, startModel, startYear, startColor, startMileage);
+	}
+}
